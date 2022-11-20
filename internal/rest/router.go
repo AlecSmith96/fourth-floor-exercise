1743d2b53ae81965bc66b9753de9ed58f8538727
Alec Smith <rcsmith.alec@gmail.com>
2022-11-20 19:38:02 +0000

    added default routes for 404 and 405 requests

--- internal/rest/router.go
+++ internal/rest/router.go
@@ -1,6 +1,7 @@
 package rest
 
 import (
+	"net/http"
 	"time"
 
 	"github.com/AlecSmith96/fourth-floor-exercise/internal/adapters"
@@ -18,6 +19,20 @@ func NewRouter(config *entities.ConfigRest, logger *zap.Logger, twitchAdapter ad
 	// use injected logger for logging and recovery in gin router
 	router.Use(ginzap.Ginzap(logger, time.RFC3339, true))
 	router.Use(ginzap.RecoveryWithZap(logger, true))
+	
+	// default routes for NotFound and MethodNotAllowed
+	router.NoMethod(func(c *gin.Context) {
+		c.JSON(http.StatusMethodNotAllowed, gin.H{
+			"code": 405,
+			"message": "method not allowed",
+		})
+	})
+	router.NoRoute(func(c *gin.Context) {
+		c.JSON(http.StatusNotFound, gin.H{
+			"code": 404,
+			"message": "page not found",
+		})
+	})
 
 	router.GET("/videos/:userID", func(c *gin.Context) {
 		ViewHandler(c, logger, twitchAdapter, analyticsAdapter)
